8f0e811543dacfbe7f529aa7fce1beb8d601a900
Joana <joanamaite0@gmail.com>
2023-08-09 14:50:33 -0300

    Ajustando requisição

--- js/scriptReq.js
+++ js/scriptReq.js
@@ -11,6 +11,25 @@ fetch('https://api-rest-post-diegocandido.herokuapp.com/postagens/').then(respon
     let postagImg = `https://api-rest-post-diegocandido.herokuapp.com${result[0].thumbImage}`;
     document.querySelector(".image").src = postagImg;
 
+    document.querySelector(".text_card1").innerHTML = result[2].description;
+    document.querySelector(".date_card1").innerHTML = result[2].postDate;
+    document.querySelector(".title_card1").innerHTML = result[2].title;
+    let postagImgCard2 = `https://api-rest-post-diegocandido.herokuapp.com${result[2].thumbImage}`;
+    document.querySelector(".imagem_card2").src = postagImgCard2;
+
+    document.querySelector(".text_card3").innerHTML = result[5].description;
+    document.querySelector(".date_card3").innerHTML = result[5].postDate;
+    document.querySelector(".title_card3").innerHTML = result[5].title;
+    let postagImgCard3 = `https://api-rest-post-diegocandido.herokuapp.com${result[5].thumbImage}`;
+    document.querySelector(".imagem_card3").src = postagImgCard3;
+
+    document.querySelector(".text_card").innerHTML = result[1].description;
+    document.querySelector(".date_card").innerHTML = result[1].postDate;
+    document.querySelector(".titlle_card").innerHTML = result[1].title;
+    let postagImgCard = `https://api-rest-post-diegocandido.herokuapp.com${result[1].thumbImage}`;
+    document.querySelector(".imagem_card1").src = postagImgCard;
+
+
     let postagImg1 = `https://api-rest-post-diegocandido.herokuapp.com${result[4].thumbImage}`;
     document.querySelector(".banner").style.backgroundImage =`url(${postagImg1})`;
 
