05d0cea1882b153ccad723bcf90797612fe4b3bd
Joana <Joana-mrodrigues2@educar.rs.gov.br>
2023-08-14 11:36:19 -0300

    Colocando botao de saber mais

---
+++ js/scriptPostUnico.js
@@ -0,0 +1,8 @@
+let container = document.querySelector(".post-unico");
+let imagem = document.querySelector(".image");
+let titulo = document.querySelector(".title");
+let text= document.querySelector(".text");
+
+
+
+
